23b6d095421611d35dca755fd416093f68dd4035
Mohammed Zaid Shaikh <mohammed.shaikh1@ucalgary.ca>
2024-03-20 18:49:31 -0600

    src and public folders for react website

---
+++ src/component/Homepage.js
@@ -0,0 +1,9 @@
+import Header from './Header'
+import HomeMainSection from './HomeMainSection'
+import Footer from './Footer'
+
+<div>
+ <Header />
+ <HomeMainSection />
+ <Footer />
+</div>
---
+++ src/index.js
@@ -0,0 +1,19 @@
+// Created by: Mohammed Zaid Shaikh (UCID: 30173041) and Adam Yuen (UCID: 10187125)
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
